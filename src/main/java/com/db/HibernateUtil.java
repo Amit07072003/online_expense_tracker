bfe1ab137e3117b9802a31d47588d121f3fa07c8
Amit kumar <133324132+Amit07072003@users.noreply.github.com>
2024-05-17 15:29:09 +0530

    Update HibernateUtil.java

--- src/main/java/com/db/HibernateUtil.java
+++ src/main/java/com/db/HibernateUtil.java
@@ -23,10 +23,10 @@ public class HibernateUtil {
 				Properties properties = new Properties();
 
 				properties.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-				properties.put(Environment.URL, "jdbc:mysql://localhost:3306/expense_tracker_db");
-				properties.put(Environment.USER, "root");
-				properties.put(Environment.PASS, "Laxman07072003//");
-				properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+				properties.put(Environment.URL, "jdbc:postgresql://localhost:5432/expense_tracker_db_hadk");
+				properties.put(Environment.USER, "expense_tracker_db_hadk_user");
+				properties.put(Environment.PASS, "3G6cXby1fhbC9lqzzvb6xwQ4F09Y1in2");
+				properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
 				properties.put(Environment.HBM2DDL_AUTO, "update");
 				properties.put(Environment.SHOW_SQL, true);
 
